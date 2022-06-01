7e3433727c7f11428ad8551802dfedbd664ad29d
atana <atanas_sd@abv.bg>
2022-06-02 01:13:16 +0300

    clearing the code

--- scripts/surveyBuilder.js
+++ scripts/surveyBuilder.js
@@ -7,12 +7,6 @@ const creator = new SurveyCreator.SurveyCreator(creatorOptions);
 creator.saveSurveyFunc = (saveNo, callback) => { 
     window.localStorage.setItem("survey-json", creator.text);
     callback(saveNo, true);
-    // saveSurveyJson(
-    //     "https://your-web-service.com/",
-    //     creator.JSON,
-    //     saveNo,
-    //     callback
-    // );
 };
 
 document.addEventListener("DOMContentLoaded", function() {
